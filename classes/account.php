283083c7146e59e974ebf220e172a1259f8f7216
Marco Oliveira <marcomoa@hotmail.com>
2017-10-12 17:38:27 -0300

    Premium points agora entregam coins

--- classes/account.php
+++ classes/account.php
@@ -8,8 +8,8 @@ class Account extends ObjectData
 	const LOADTYPE_NAME = 'name';
 	const LOADTYPE_MAIL = 'email';
 	public static $table = 'accounts';
-	public $data = array('name' => null, 'password' => null, 'premdays' => null, 'coins' => null, 'lastday' => null, 'email' => null, 'key' => null, 'create_ip' => null, 'creation' => null, 'premium_points' => null, 'page_access' => null, 'location' => null, 'rlname' => null, 'birth_date' => null, 'gender' => null, 'email_new' => null, 'email_new_time' => null, 'email_code' => null, 'next_email' => null, 'last_post' => null, 'flag' => null, 'vote' => null, 'loyalty_points' => null, 'guild_points' => null);
-	public static $fields = array('id', 'name', 'password', 'premdays', 'coins', 'lastday', 'email', 'key', 'create_ip', 'creation', 'premium_points', 'page_access', 'location', 'rlname','birth_date', 'gender', 'email_new', 'email_new_time', 'email_code', 'next_email', 'last_post', 'flag', 'vote', 'loyalty_points', 'guild_points');
+	public $data = array('name' => null, 'password' => null, 'premdays' => null, 'coins' => null, 'lastday' => null, 'email' => null, 'key' => null, 'create_ip' => null, 'creation' => null, 'page_access' => null, 'location' => null, 'rlname' => null, 'birth_date' => null, 'gender' => null, 'email_new' => null, 'email_new_time' => null, 'email_code' => null, 'next_email' => null, 'last_post' => null, 'flag' => null, 'vote' => null, 'loyalty_points' => null, 'guild_points' => null);
+public static $fields = array('id', 'name', 'password', 'premdays', 'coins', 'lastday', 'email', 'key', 'create_ip', 'creation', 'page_access', 'location', 'rlname','birth_date', 'gender', 'email_new', 'email_new_time', 'email_code', 'next_email', 'last_post', 'flag', 'vote', 'loyalty_points', 'guild_points');
 	public $players;
 	public $playerRanks;
 	public $guildAccess;
@@ -205,8 +205,8 @@ class Account extends ObjectData
 	public function getCreateIP(){return $this->data['create_ip'];}
 	public function setCreateDate($value){$this->data['creation'] = $value;}
 	public function getCreateDate(){return $this->data['creation'];}
-	public function setPremiumPoints($value){$this->data['premium_points'] = $value;}
-	public function getPremiumPoints(){return $this->data['premium_points'];}
+	public function setPremiumPoints($value){$this->data['coins'] = $value;}
+	public function getPremiumPoints(){return $this->data['coins'];}
     public function setCoins($value){$this->data['coins'] = $value;}
     public function getCoins(){return $this->data['coins'];}
 
